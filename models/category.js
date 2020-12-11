9c905c94b3770619a5cf5f062c82c0c06cd73e10
Jessie Chen <yhcjessie1@gmail.com>
2020-12-11 06:35:08 -0500

    first commit

---
+++ models/category.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const categorySchema = Schema({
+  name: { type: String, required: true },
+  phrases: [
+    { type: String, required: true }
+  ],
+})
+module.exports = mongoose.model('Category', categorySchema)
---
+++ seed.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const Category = require('./models/category');
+
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ 'project4';
+mongoose.connect(
+    MONGODB_URI,
+  { useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex: true,
+    useFindAndModify:false,});
+  () => {
+    console.log('the connection with mongod is established');
+  };
+
+async function seed() {
+
+  // CREATE A NEW Category
+  const BeforeandAfter = new Category({
+    name: 'Before and After',
+    phrases:['Whitney Houston Texas', 'Stop Sign Language']
+    
+  });
+
+  const BestSeller= new Category({
+    name: 'Best Sellers',
+    phrases:['Whitney Houston Texas', 'Old Yeller']
+    
+  });
+
+  
+  BeforeandAfter.save(function (err, saved) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Before and After is ', saved);
+    }
+  });
+  
+  
+  BestSeller.save(function (err, saved) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Best Seller is ', saved);
+    }
+  });
+}
+
+seed();
