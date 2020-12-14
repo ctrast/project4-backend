3a31590587cc4d4d4e83eb1af0062068cbf72f97
Cory Trast <cory_trast@hotmail.com>
2020-12-14 09:05:58 -0600

    updated seed data and reseed AWS instance

--- models/category.js
+++ models/category.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  phrases: [{ type: Array, required: true }],
+  phrases: [
+      { type: Array, required: true },
+  ],
 });
 
 module.exports = mongoose.model("Category", categorySchema);
